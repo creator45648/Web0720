95f76022df91219ead0237097d778dcf13614cd3
ChengHau <popo45648@hotmail.com>
2025-07-17 22:47:05 +0800

    建立View

--- Web0720.AppHost/Program.cs
+++ Web0720.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Web_API>("web-api");
 
+builder.AddProject<Projects.Web_View>("web-view");
+
 builder.Build().Run();
