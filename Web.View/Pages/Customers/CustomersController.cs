8fab344725e17e9b787bdffb1d224264f97cf3c0
ChengHau <popo45648@hotmail.com>
2025-07-20 01:19:49 +0800

    加入顧客資料檢視畫面初始

---
+++ Web.View/Pages/Customers/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Web.API.Client;
+
+namespace Web.View.Pages.Customers
+{
+    public class CustomersController : Controller
+    {
+        private readonly View.IWebApiClient<string, Customer> webApiClient;
+
+        public CustomersController(IWebApiClient<string, Customer> webApiClient)
+        {
+            this.webApiClient = webApiClient;
+        }
+        // GET: CustomersController
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
