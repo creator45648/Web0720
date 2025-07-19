1b12aa7b3c1ef133fd5c0d353056b4961d4fd65f
ChengHau <popo45648@hotmail.com>
2025-07-20 01:18:33 +0800

    新增 CustomerApiClient 類別及 IWebApiClient 介面
    實作了 `CustomerApiClient` 類別，提供客戶的 CRUD 操作方法，包括 `CreateAsync`、`DeleteAsync`、`GetByIdAsync`、`GetAsync` 和 `UpdateAsync`。同時新增了 `IWebApiClient` 介面，定義與 Web API 互動的基本方法。

---
+++ Web.View/CustomerApiClient.cs
@@ -0,0 +1,41 @@
+﻿using Web.API.Client;
+
+namespace Web.View
+{
+    public class CustomerApiClient : IWebApiClient<string, Customer>
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _baseUrl = "https://localhost:5001/api/";
+
+        public CustomerApiClient(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        swaggerClient _client => new swaggerClient(_baseUrl, _httpClient);
+        //Customers
+        public async Task CreateAsync(Customer customer)
+        {
+            await _client.CustomersPOSTAsync(customer);
+        }
+
+        public async Task DeleteAsync(string id)
+        {
+            await _client.CustomersDELETEAsync(id);
+        }
+
+        public async Task<Customer> GetByIdAsync(string id)
+        {
+            return await _client.CustomersGETAsync(id);
+        }
+
+        public async Task<IEnumerable<Customer>> GetAsync()
+        {
+            return await _client.CustomersAllAsync();
+        }
+
+        public async Task UpdateAsync(string id, Customer customer)
+        {
+            await _client.CustomersPUTAsync(id, customer);
+        }
+    }
+}
---
+++ Web.View/IWebApiClient.cs
@@ -0,0 +1,16 @@
+﻿namespace Web.View
+{
+    /// <summary>
+    /// 建立swaggerClient 介面
+    /// </summary>
+    public interface IWebApiClient<Key, Model>
+    {
+        // Define methods for interacting with the web API here
+        // For example:
+        Task<IEnumerable<Model>> GetAsync();
+        Task<Model> GetByIdAsync(Key id);
+        Task CreateAsync(Model customer);
+        Task UpdateAsync(Key id, Model customer);
+        Task DeleteAsync(Key id);
+    }
+}
